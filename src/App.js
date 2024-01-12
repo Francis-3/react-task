9ba547a1eaba92ff82d7f2b2e46c040ec38f2d03
Francis-3 <francisco.villarin.cruz@gmail.com>
2024-01-12 19:53:25 +0800

    Feat: for react task updates

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import './App.css';
+import axios from 'axios';
+var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=RIBXT3XYLI69PC0Q';
+const { 'Meta Data': data1, 'Time Series (5min)': data2 } = await axios.get(url).then(res => res.data);
+function App() {
+  return (
+    <div className="container">
+      <div class='m-4'>
+        {
+          Object.keys(data1).map((key, idx) => (
+            <div class="d-flex justify-self-start" key={idx}><label class='font-bold'>{key}: </label> {data1[key]}</div>
+          ))
+        }
+      </div>
+
+      {/* {
+        Object.keys(data2).map((key, idx) => (
+          <h4 key={idx}>{key}
+            {
+              Object.keys(data2[key]).map((innerKey, innerId) => (
+                <p key={innerId}>{innerKey}: {data2[key][innerKey]}</p>
+              ))
+            }
+          </h4>
+        ))
+      } */}
+
+
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-4">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Time Series (5min)
+              </th>
+              <th scope="col" class="px-3 py-3 bg-gray-50 dark:bg-gray-800">
+                Open
+              </th>
+              <th scope="col" class="px-3 py-3 bg-gray-50 dark:bg-gray-800">
+                High
+              </th>
+              <th scope="col" class="px-3 py-3 bg-gray-50 dark:bg-gray-800">
+                Low
+              </th>
+              <th scope="col" class="px-3 py-3 bg-gray-50 dark:bg-gray-800">
+                Close
+              </th>
+              <th scope="col" class="px-3 py-3 bg-gray-50 dark:bg-gray-800">
+                Volume
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              Object.keys(data2).map((key, idx) => (
+                <tr class="border-b border-gray-200 dark:border-gray-700" key={idx}>
+                  <td class="px-3 py-4">
+                    {key}
+                  </td>
+                  {
+                    Object.keys(data2[key]).map((innerKey, innerId) => (
+                      <td class="px-3 py-4" key={innerId}>
+                        {data2[key][innerKey]}
+                      </td>
+                    ))
+                  }
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
